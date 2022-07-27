b6bee455f16a7f4d044eb6abece1eada40b20467
Jaroslav Knapek <knapejar@fit.cvut.cz>
2022-07-27 11:23:45 +0200

    Camera preview and sensor array added

--- previewSensors.py
+++ previewSensors.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, Response
 import random    
 
 from core.regulator import AverageFilter
 
+from camera_pi import Camera
 
 
-mockData = True
+mockData = False
+
 
 
 sa = None
@@ -15,6 +17,7 @@ if not mockData:
 
 def readSensorArray(mockData = False):
     i = 3 #Posun senzorů vpravo
+    if not mockData: sa.read()
     for x in range(12):
         if mockData:
             val = random.randint(0, 1250)
@@ -49,14 +52,27 @@ def sensors ():
 @app.route('/updateSensors')
 def update():
     sensorArray = readSensorArray(mockData=mockData)
-    telemetry = {"height": 0, "battery": 0, "time": 0, "speed": 0, "state": "Offline"}
+    telemetry = {"height": 0, "battery": 0, "time": 0, "speed": 0, "state": "Online"}
     toSend = {"telemetry": telemetry, "sensorArray": sensorArray}
     return jsonify(toSend)
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 
 if __name__ == '__main__':
     data = []
     for i in range(12):
-        data.append(AverageFilter(size = 5))
+        data.append(AverageFilter(size = 2))
     app.run(host='0.0.0.0', port=80)
